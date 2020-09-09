cb3f993267a5b2f6dc11873208f40ac525ec39c4
GenaroRocha <67811130+GenaroRocha@users.noreply.github.com>
2020-09-08 19:39:46 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,13 @@
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl, "./Data/Power.zip", method="curl")
+unzip("./Data/Power.zip")
+data <- read.table("household_power_consumption.txt", skip=1, sep=";")
+names(data) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering3")
+subdata <- subset(data, data$Date == "1/2/2007" |  data$Date == "2/2/2007")
+remove(data)
+data <- subdata
+remove(subdata)
+data$Global_active_power <- as.numeric(data$Global_active_power)
+png("plot1.png", width=480, height=480, units="px")
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,16 @@
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl, "./Data/Power.zip", method="curl")
+unzip("./Data/Power.zip")
+data <- read.table("household_power_consumption.txt", skip=1, sep=";")
+names(data) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering3")
+subdata <- subset(data, data$Date == "1/2/2007" |  data$Date == "2/2/2007")
+remove(data)
+data <- subdata
+remove(subdata)
+data$time <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+png("plot3.png", width=480, height=480, units="px")
+plot(data$time, data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(data$time, data$Sub_metering3, col="blue")
+lines(data$time, data$Sub_metering_2, col="red")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1, cex=0.6)
+dev.off()
